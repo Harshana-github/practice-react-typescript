f5db9ac691f30c6c7d21290f821b66ab2b068650
Harshana-50 <harshana@konnect.team>
2023-11-10 11:21:43 +0530

    changed-function-type-in-Greet-componnet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Greet from "./components/Greet";
+import { Greet } from "./components/Greet";
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Harshana' />
+      <Greet name="Harshana" />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,13 +1,11 @@
 type GreetProps = {
-    name: string
-}
+  name: string;
+};
 
-const Greet = (props: GreetProps) => {
+export const Greet = (props: GreetProps) => {
   return (
     <div>
       <h2>Welcome {props.name}! You have 10 unread messages</h2>
     </div>
-  )
-}
-
-export default Greet
+  );
+};
